fe037376c6a1b4eb0d8ee995036db5fb21d8bea7
Sharad Yadav <sharady@nvidia.com>
2021-01-15 11:33:35 +0530

    Add nvidia system dump changes.
    1. Add nvidia system dump extension to collect system logs.
    2. Update createDump to accept additional arguments
       as <key,value> pairs.
    3. Update dreport tool to support system type dump.
    4. Update dreport to accept system dump arguments
       and pass on to plugins.
    
    Tested bmc and system dump redfish APIs:
    1. Dump create
       curl -k -H "X-Auth-Token: $token" -H 'Content-Type: application/json' -X POST -d '{"DiagnosticDataType":"Manager"}'
        https://${bmc}/redfish/v1/Managers/bmc/LogServices/Dump/Actions/LogService.CollectDiagnosticData
    {
      "@odata.id": "/redfish/v1/TaskService/Tasks/0",
      "@odata.type": "#Task.v1_4_3.Task",
      "Id": "0",
      "TaskState": "Running",
      "TaskStatus": "OK"
    }
    
       curl -k -H "X-Auth-Token: $token" -H 'Content-Type: application/json' -X POST -d '{"DiagnosticDataType":"OEM",
        "OEMDiagnosticDataType":"System;bf_ip=<ip_address>;bf_username=<username>;bf_password=<password>"}'
        https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Actions/LogService.CollectDiagnosticData
    {
      "@odata.id": "/redfish/v1/TaskService/Tasks/1",
      "@odata.type": "#Task.v1_4_3.Task",
      "Id": "1",
      "TaskState": "Running",
      "TaskStatus": "OK"
    }
    
    2. Dump lists
       curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X
        GET https://${bmc}/redfish/v1/Managers/bmc/LogServices/Dump/Entries
    {
      "@odata.id": "/redfish/v1/Managers/bmc/LogServices/Dump/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of BMC Dump Entries",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Managers/bmc/LogServices/Dump/Entries/26",
          "@odata.type": "#LogEntry.v1_7_0.LogEntry",
          "AdditionalDataSizeBytes": 62520,
          "AdditionalDataURI": "/redfish/v1/Managers/bmc/LogServices/Dump/attachment/26",
          "Created": "2021-02-11T08:55:12+00:00",
          "DiagnosticDataType": "Manager",
          "EntryType": "Event",
          "Id": "26",
          "Name": "BMC Dump Entry"
        }
      ],
      "Members@odata.count": 2,
      "Name": "BMC Dump Entries"
    }
    
       curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X
        GET https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/Dump/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of System Dump Entries",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/Dump/Entries/1",
          "@odata.type": "#LogEntry.v1_7_0.LogEntry",
          "AdditionalDataSizeBytes": 320,
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/Dump/attachment/1",
          "Created": "2021-02-11T08:57:20+00:00",
          "DiagnosticDataType": "OEM",
          "EntryType": "Event",
          "Id": "1",
          "Name": "System Dump Entry",
          "OEMDiagnosticDataType": "System"
        }
      ],
      "Members@odata.count": 1,
      "Name": "System Dump Entries"
    }
    
    3. Dump delete
      curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X
       DELETE https://${bmc}/redfish/v1/Managers/bmc/LogServices/Dump/Entries/2
    
      curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X
       DELETE https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries/2
    
    4. Dump delete all
      curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X
       POST https://${bmc}/redfish/v1/Managers/bmc/LogServices/Dump/Actions/LogService.ClearLog
    
      curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X
       POST https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Actions/LogService.ClearLog
    
    5. Dump download
      curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X
       GET https://${bmc}/redfish/v1/Managers/bmc/LogServices/Dump/attachment/1 --output dumps/dump1.tar
    
      curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X
       GET https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/attachment/1 --output dumps/dump1.tar

---
+++ dump-extensions/nvidia-dumps/dump_manager_system.cpp
@@ -0,0 +1,319 @@
+#include "config.h"
+
+#include "dump_manager_system.hpp"
+
+#include "system_dump_entry.hpp"
+#include "dump_internal.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
+#include "xyz/openbmc_project/Dump/Create/error.hpp"
+
+#include <sys/inotify.h>
+#include <unistd.h>
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <regex>
+
+namespace phosphor
+{
+namespace dump
+{
+namespace system
+{
+
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+using namespace phosphor::logging;
+
+// TODO: Merge system dump with bmc dump to avoid code duplication.
+
+void Manager::limitDumpEntries()
+{
+    // Delete dumps on reaching allowed entries
+    auto totalDumps = entries.size();
+    if (totalDumps < maxAllowedDumpEntries)
+    {
+        // Do nothing - Its within allowed entries
+        return;
+    }
+    // Get the oldest dumps
+    int excessDumps = totalDumps - (maxAllowedDumpEntries-1);
+    // Delete the oldest dumps
+    for (auto d = entries.begin(); d != entries.end() && excessDumps;
+            d++) {
+        auto& entry = d->second;
+        entry->delete_();
+        --excessDumps;
+    }
+
+    return;
+}
+
+sdbusplus::message::object_path
+    Manager::createDump(std::map<std::string, std::string> params)
+{
+    // Limit dumps to max allowed entries
+    limitDumpEntries();
+    auto id = captureDump(params);
+
+    // Entry Object path.
+    auto objPath = fs::path(baseEntryPath) / std::to_string(id);
+
+    try
+    {
+        std::time_t timeStamp = std::time(nullptr);
+        entries.insert(std::make_pair(
+            id, std::make_unique<system::Entry>(
+                    bus, objPath.c_str(), id, timeStamp, 0, std::string(),
+                    phosphor::dump::OperationStatus::InProgress, *this)));
+    }
+    catch (const std::invalid_argument& e)
+    {
+        log<level::ERR>(e.what());
+        log<level::ERR>("Error in creating system dump entry",
+                        entry("OBJECTPATH=%s", objPath.c_str()),
+                        entry("ID=%d", id));
+        elog<InternalFailure>();
+    }
+
+    return objPath.string();
+}
+
+uint32_t Manager::captureDump(std::map<std::string, std::string> params)
+{
+    // Get Dump size.
+    auto size = getAllowedSize();
+
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        fs::path dumpPath(dumpDir);
+        auto id = std::to_string(lastEntryId + 1);
+        dumpPath /= id;
+        auto sizeStr = std::to_string(size);
+
+        std::string dumpType = "system";
+
+        // Construct additional arguments from params
+        std::map<std::string, std::string>::iterator itr;
+        std::vector<std::string> addArgs;
+        for (itr = params.begin(); itr != params.end(); ++itr) {
+            addArgs.push_back(itr->first + "=" + itr->second);
+        }
+
+        // Construct dreport arguments
+        std::vector<char*> arg_v;
+        std::string fPath = "/usr/bin/dreport";
+        arg_v.push_back(&fPath[0]);
+        std::string dOption = "-d";
+        arg_v.push_back(&dOption[0]);
+        arg_v.push_back(const_cast<char*>(dumpPath.c_str()));
+        std::string iOption = "-i";
+        arg_v.push_back(&iOption[0]);
+        arg_v.push_back(const_cast<char*>(id.c_str()));
+        std::string sOption = "-s";
+        arg_v.push_back(&sOption[0]);
+        arg_v.push_back(const_cast<char*>(sizeStr.c_str()));
+        std::string qOption = "-q";
+        arg_v.push_back(&qOption[0]);
+        std::string vOption = "-v";
+        arg_v.push_back(&vOption[0]);
+        std::string tOption = "-t";
+        arg_v.push_back(&tOption[0]);
+        arg_v.push_back(const_cast<char*>(dumpType.c_str()));
+        // Add additional arguments
+        std::string aOption = "-a";
+        for(int i=0; i < (int) addArgs.size(); i++)
+        {
+            arg_v.push_back(&aOption[0]);
+            arg_v.push_back(const_cast<char*>((addArgs.at(i)).c_str()));
+        }
+        arg_v.push_back(nullptr);
+
+        execv(arg_v[0], &arg_v[0]);
+
+        // dreport script execution is failed.
+        auto error = errno;
+        log<level::ERR>("System dump: Error occurred during dreport function execution",
+                        entry("ERRNO=%d", error));
+        elog<InternalFailure>();
+    }
+    else if (pid > 0)
+    {
+        auto rc = sd_event_add_child(eventLoop.get(), nullptr, pid,
+                                     WEXITED | WSTOPPED, callback, nullptr);
+        if (0 > rc)
+        {
+            // Failed to add to event loop
+            log<level::ERR>("System dump: Error occurred during the sd_event_add_child call",
+                            entry("RC=%d", rc));
+            elog<InternalFailure>();
+        }
+    }
+    else
+    {
+        auto error = errno;
+        log<level::ERR>("System dump: Error occurred during fork", entry("ERRNO=%d", error));
+        elog<InternalFailure>();
+    }
+
+    return ++lastEntryId;
+}
+
+void Manager::createEntry(const fs::path& file)
+{
+    // Dump File Name format obmcdump_ID_EPOCHTIME.EXT
+    static constexpr auto ID_POS = 1;
+    static constexpr auto EPOCHTIME_POS = 2;
+    std::regex file_regex("obmcdump_([0-9]+)_([0-9]+).([a-zA-Z0-9]+)");
+
+    std::smatch match;
+    std::string name = file.filename();
+
+    if (!((std::regex_search(name, match, file_regex)) && (match.size() > 0)))
+    {
+        log<level::ERR>("System dump: Invalid Dump file name",
+                        entry("FILENAME=%s", file.filename().c_str()));
+        return;
+    }
+
+    auto idString = match[ID_POS];
+    auto msString = match[EPOCHTIME_POS];
+
+    auto id = stoul(idString);
+
+    // If there is an existing entry update it and return.
+    auto dumpEntry = entries.find(id);
+    if (dumpEntry != entries.end())
+    {
+        dynamic_cast<phosphor::dump::system::Entry*>(dumpEntry->second.get())
+            ->update(stoull(msString), fs::file_size(file), file);
+        return;
+    }
+
+    // Entry Object path.
+    auto objPath = fs::path(baseEntryPath) / std::to_string(id);
+
+    try
+    {
+        entries.insert(
+            std::make_pair(id, std::make_unique<system::Entry>(
+                bus, objPath.c_str(), id, stoull(msString), fs::file_size(file),
+                file, phosphor::dump::OperationStatus::Completed, *this)));
+
+    }
+    catch (const std::invalid_argument& e)
+    {
+        log<level::ERR>(e.what());
+        log<level::ERR>("Error in creating system dump entry",
+                        entry("OBJECTPATH=%s", objPath.c_str()),
+                        entry("ID=%d", id),
+                        entry("TIMESTAMP=%ull", stoull(msString)),
+                        entry("SIZE=%d", fs::file_size(file)),
+                        entry("FILENAME=%s", file.c_str()));
+        return;
+    }
+}
+
+void Manager::watchCallback(const UserMap& fileInfo)
+{
+    for (const auto& i : fileInfo)
+    {
+        // For any new dump file create dump entry object
+        // and associated inotify watch.
+        if (IN_CLOSE_WRITE == i.second)
+        {
+            removeWatch(i.first);
+
+            createEntry(i.first);
+        }
+        // Start inotify watch on newly created directory.
+        else if ((IN_CREATE == i.second) && fs::is_directory(i.first))
+        {
+            auto watchObj = std::make_unique<Watch>(
+                eventLoop, IN_NONBLOCK, IN_CLOSE_WRITE, EPOLLIN, i.first,
+                std::bind(
+                    std::mem_fn(&phosphor::dump::system::Manager::watchCallback),
+                    this, std::placeholders::_1));
+
+            childWatchMap.emplace(i.first, std::move(watchObj));
+        }
+    }
+}
+
+void Manager::removeWatch(const fs::path& path)
+{
+    // Delete Watch entry from map.
+    childWatchMap.erase(path);
+}
+
+void Manager::restore()
+{
+    fs::path dir(dumpDir);
+    if (!fs::exists(dir) || fs::is_empty(dir))
+    {
+        return;
+    }
+
+    // Dump file path: <DUMP_PATH>/<id>/<filename>
+    for (const auto& p : fs::directory_iterator(dir))
+    {
+        auto idStr = p.path().filename().string();
+
+        // Consider only directory's with dump id as name.
+        // Note: As per design one file per directory.
+        if ((fs::is_directory(p.path())) &&
+            std::all_of(idStr.begin(), idStr.end(), ::isdigit))
+        {
+            lastEntryId =
+                std::max(lastEntryId, static_cast<uint32_t>(std::stoul(idStr)));
+            auto fileIt = fs::directory_iterator(p.path());
+            // Create dump entry d-bus object.
+            if (fileIt != fs::end(fileIt))
+            {
+                createEntry(fileIt->path());
+            }
+        }
+    }
+}
+
+size_t Manager::getAllowedSize()
+{
+    using namespace sdbusplus::xyz::openbmc_project::Dump::Create::Error;
+    using Reason = xyz::openbmc_project::Dump::Create::QuotaExceeded::REASON;
+
+    auto size = 0;
+
+    // Get current size of the dump directory.
+    for (const auto& p : fs::recursive_directory_iterator(dumpDir))
+    {
+        if (!fs::is_directory(p))
+        {
+            size += fs::file_size(p);
+        }
+    }
+
+    // Convert size into KB
+    size = size / 1024;
+
+    // Set the Dump size to Maximum  if the free space is greater than
+    // Dump max size otherwise return the available size.
+
+    size = (size > SYSTEM_DUMP_TOTAL_SIZE ? 0 : SYSTEM_DUMP_TOTAL_SIZE - size);
+
+    if (size < SYSTEM_DUMP_MIN_SPACE_REQD)
+    {
+        // Reached to maximum limit
+        elog<QuotaExceeded>(Reason("Not enough space: Delete old dumps"));
+    }
+    if (size > SYSTEM_DUMP_MAX_SIZE)
+    {
+        size = SYSTEM_DUMP_MAX_SIZE;
+    }
+
+    return size;
+}
+
+} // namespace system
+} // namespace dump
+} // namespace phosphor
---
+++ dump-extensions/nvidia-dumps/dump_manager_system.hpp
@@ -0,0 +1,148 @@
+#pragma once
+
+#include "dump_manager.hpp"
+#include "dump_utils.hpp"
+#include "watch.hpp"
+#include "xyz/openbmc_project/Dump/NewDump/server.hpp"
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <experimental/filesystem>
+#include <xyz/openbmc_project/Dump/Create/server.hpp>
+
+namespace phosphor
+{
+namespace dump
+{
+namespace system
+{
+
+using CreateIface = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Dump::server::Create>;
+
+using UserMap = phosphor::dump::inotify::UserMap;
+
+namespace fs = std::experimental::filesystem;
+
+using Watch = phosphor::dump::inotify::Watch;
+
+// Max dump entries allowed
+static const uint8_t maxAllowedDumpEntries = 2;
+
+/** @class Manager
+ *  @brief OpenBMC Dump manager implementation.
+ *  @details A concrete implementation for the
+ *  xyz.openbmc_project.Dump.Create DBus API
+ */
+class Manager : virtual public CreateIface,
+                virtual public phosphor::dump::Manager
+{
+  public:
+    Manager() = delete;
+    Manager(const Manager&) = default;
+    Manager& operator=(const Manager&) = delete;
+    Manager(Manager&&) = delete;
+    Manager& operator=(Manager&&) = delete;
+    virtual ~Manager() = default;
+
+    /** @brief Constructor to put object onto bus at a dbus path.
+     *  @param[in] bus - Bus to attach to.
+     *  @param[in] event - Dump manager sd_event loop.
+     *  @param[in] path - Path to attach at.
+     *  @param[in] baseEntryPath - Base path for dump entry.
+     *  @param[in] filePath - Path where the dumps are stored.
+     */
+    Manager(sdbusplus::bus::bus& bus, const EventPtr& event, const char* path,
+            const std::string& baseEntryPath, const char* filePath) :
+        CreateIface(bus, path),
+        phosphor::dump::Manager(bus, path, baseEntryPath),
+        eventLoop(event.get()),
+        dumpWatch(
+            eventLoop, IN_NONBLOCK, IN_CLOSE_WRITE | IN_CREATE, EPOLLIN,
+            filePath,
+            std::bind(std::mem_fn(&phosphor::dump::system::Manager::watchCallback),
+                      this, std::placeholders::_1)),
+        dumpDir(filePath)
+    {
+    }
+
+    /** @brief Implementation of dump watch call back
+     *  @param [in] fileInfo - map of file info  path:event
+     */
+    void watchCallback(const UserMap& fileInfo);
+
+    /** @brief Construct dump d-bus objects from their persisted
+     *        representations.
+     */
+    void restore() override;
+
+    /** @brief Implementation for CreateDump
+     *  Method to create system dump.
+     *
+     *  @return object_path - The object path of the new dump entry.
+     */
+    sdbusplus::message::object_path
+        createDump(std::map<std::string, std::string> params) override;
+
+  private:
+    /** @brief Create Dump entry d-bus object
+     *  @param[in] fullPath - Full path of the Dump file name
+     */
+    void createEntry(const fs::path& fullPath);
+
+    /** @brief Capture System Dump.
+     *  @param[in] parama - Additional arguments for system dump.
+     *  @return id - The Dump entry id number.
+     */
+    uint32_t captureDump(std::map<std::string, std::string> params);
+
+    /** @brief sd_event_add_child callback
+     *
+     *  @param[in] s - event source
+     *  @param[in] si - signal info
+     *  @param[in] userdata - pointer to Watch object
+     *
+     *  @returns 0 on success, -1 on fail
+     */
+    static int callback(sd_event_source*, const siginfo_t*, void*)
+    {
+        // No specific action required in
+        // the sd_event_add_child callback.
+        return 0;
+    }
+    /** @brief Remove specified watch object pointer from the
+     *        watch map and associated entry from the map.
+     *        @param[in] path - unique identifier of the map
+     */
+    void removeWatch(const fs::path& path);
+
+    /** @brief Calculate per dump allowed size based on the available
+     *        size in the dump location.
+     *  @returns dump size in kilobytes.
+     */
+    size_t getAllowedSize();
+
+    /** @brief sdbusplus Dump event loop */
+    EventPtr eventLoop;
+
+    /** @brief Dump main watch object */
+    Watch dumpWatch;
+
+    /** @brief Path to the dump file*/
+    std::string dumpDir;
+
+    /** @brief Child directory path and its associated watch object map
+     *        [path:watch object]
+     */
+    std::map<fs::path, std::unique_ptr<Watch>> childWatchMap;
+
+    /** @brief Erase BMC dump entry and delete respective dump file
+     *         from permanent location on reaching maximum allowed
+     *         entries.
+     */
+    void limitDumpEntries();
+};
+
+} // namespace system
+} // namespace dump
+} // namespace phosphor
