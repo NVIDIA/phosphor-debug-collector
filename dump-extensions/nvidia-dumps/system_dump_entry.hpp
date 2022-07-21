667759f02f2025a1b04579db03a02003fd85ef99
Deepak Kodihalli <deepak.kodihalli.83@gmail.com>
2022-07-21 19:54:06 +0530

    upstream-sync: react to sdbusplus signal emit API change
    Fixes JIRA https://
    
    Signed-off-by: Deepak Kodihalli <deepak.kodihalli.83@gmail.com>

--- dump-extensions/nvidia-dumps/system_dump_entry.hpp
+++ dump-extensions/nvidia-dumps/system_dump_entry.hpp
@@ -55,7 +55,7 @@ class Entry : virtual public EntryIfaces, virtual public phosphor::dump::Entry
           uint64_t timeStamp, uint64_t fileSize, const fs::path& file,
           phosphor::dump::OperationStatus status,
           phosphor::dump::Manager& parent) :
-        EntryIfaces(bus, objPath.c_str(), true),
+        EntryIfaces(bus, objPath.c_str(), EntryIfaces::action::defer_emit),
         phosphor::dump::Entry(bus, objPath.c_str(), dumpId, timeStamp, fileSize,
                               status, parent),
         file(file)
