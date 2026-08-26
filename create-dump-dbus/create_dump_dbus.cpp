beef624a60492da72862183a5e26a6ef84a067cb
Prithvi Pai <ppai@nvidia.com>
2026-08-26 20:18:13 +0530

    create-dump-dbus: clang-format-21 include ordering
    Fixes jira https://upstream-sync

--- create-dump-dbus/create_dump_dbus.cpp
+++ create-dump-dbus/create_dump_dbus.cpp
@@ -14,10 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <cstring>
 #include "create_dump_dbus.hpp"
 
 #include "../config.h"
+
+#include <cstring>
 
 #define FMT_HEADER_ONLY
 
